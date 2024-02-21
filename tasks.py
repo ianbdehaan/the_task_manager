fbb9d54f096b517a7fa5ef05236304d246d69569
ianbdehaan <ianbertholdhaan@gmail.com>
2024-02-21 10:51:25 +0100

    new improvements to calendar

--- tasks.py
+++ tasks.py
@@ -26,10 +26,18 @@ class Tasks:
             match len(received_dates[i].split('-')):
                 case 1:
                     today = dt.date.today()
-                    dates[i] = '{:2s}-{:02d}-{:4d}'.format(received_dates[i], today.month, today.year)
+                    if today.day <= int(received_dates[i]):
+                        dates[i] = '{:2s}-{:02d}-{:4d}'.format(received_dates[i], today.month, today.year)
+                    elif today.month == 12:
+                        dates[i] = '{:2s}-{:02d}-{:4d}'.format(received_dates[i], 1, today.year)
+                    else:
+                        dates[i] = '{:2s}-{:02d}-{:4d}'.format(received_dates[i], today.month + 1, today.year)
                 case 2:
                     today = dt.date.today()
-                    dates[i] = f"{received_dates[i]}-{today.year}"
+                    if today.month > int(received_dates[i].split('-')[1]):
+                        dates[i] = f"{received_dates[i]}-{today.year+1}"
+                    else:
+                        dates[i] = f"{received_dates[i]}-{today.year}"
                 case 3:
                     dates[i] = received_dates[i]
         self.tasks.append([name, dates[0], dates[1], dates[2], None, None])
@@ -49,17 +57,27 @@ class Tasks:
     def display(self, done = 'ignore', sort_by = 2):
         '''display the tasks in usefull ways'''
         match done:
-            case '':
-                return tabulate(sorted(self.tasks[1:], key = lambda column: column[sort_by]), headers = self.tasks[0])
+            case 'normal':
+                '''display in the chosen sorting ignoring if tasks are marked or not'''
+                return tabulate(sorted(self.tasks[1:], 
+                                key = lambda date: ''.join(date[sort_by].split('-')[::-1])),
+                                 headers = self.tasks[0])
             case 'last':
-                sorted_not_done = sorted([task for task in self.tasks[1:] if task[4] != 'x'], key = lambda column: column[sort_by])
-                sorted_marked = sorted([task for task in self.tasks[1:] if task[4] == 'x'], key = lambda column: column[sort_by])
+                '''marked tasks appear last'''
+                sorted_not_done = sorted([task for task in self.tasks[1:] if task[4] != 'x'], 
+                                         key = lambda date: ''.join(date[sort_by].split('-')[::-1]))
+                sorted_marked = sorted([task for task in self.tasks[1:] if task[4] == 'x'], 
+                                       key = lambda date: ''.join(date[sort_by].split('-')[::-1]))
                 return tabulate(sorted_not_done+sorted_marked, headers=self.tasks[0])
             case 'ignore':
-                sorted_not_done = sorted([task for task in self.tasks[1:] if task[4] != 'x'], key = lambda column: column[sort_by])
+                '''marked tasks are not shown'''
+                sorted_not_done = sorted([task for task in self.tasks[1:] if task[4] != 'x'], 
+                                         key = lambda date: ''.join(date[sort_by].split('-')[::-1]))
                 return tabulate(sorted_not_done, headers=self.tasks[0])
-            case 'first':
-                sorted_marked = sorted([task for task in self.tasks[1:] if task[4] == 'x'], key = lambda column: column[5])
+            case 'only':
+                '''only marked tasks are shown'''
+                sorted_marked = sorted([task for task in self.tasks[1:] if task[4] == 'x'], 
+                                       key = lambda date: ''.join(date[sort_by].split('-')[::-1]))
                 return tabulate(sorted_marked, headers=self.tasks[0])
 
     def __repr__(self):
@@ -83,19 +101,33 @@ def main():
         f.write('Name,Start,End,Reminder,Status,Completed_in')
         f.close()
     tasks_instance = Tasks()
-    optlist, args = getopt.getopt(sys.argv[1:], 'd:m:c:t:')
+    optlist, args = getopt.getopt(sys.argv[1:], 'd:m:c:t:v')
 
     match optlist[0][0]:
         case '-d':
+            '''deleting a task'''
             tasks_instance.delete(optlist[0][1])
+            print(tasks_instance.display(done='normal'))
         case '-m':
+            '''marking a task'''
             tasks_instance.mark(optlist[0][1])
-            print(tasks_instance.display(done='first'))
+            print(tasks_instance.display(done='only',sort_by=5))
         case '-t':
+            '''create a task'''
             tasks_instance.new_task(optlist[0][1], args)
+            print(tasks_instance.display())
         case '-c':
             pass
-    print(tasks_instance.display())
+        case '-v':
+            '''visualizing the tasks'''
+            print(args)
+            if len(args)==2:
+                print('hi')
+                print(tasks_instance.display(done=args[1], sort_by=int(args[0])))
+            elif len(args)==1:
+                print(tasks_instance.display(sort_by=int(args[0])))                
+            else:
+                print(tasks_instance.display())
     tasks_instance.save()
 
 
